b8c12114091f4c55b798dab263d52d01f7807872
Integra-developer2 <developer2.it@integraa.it>
2025-09-22 10:56:29 +0200

    queryF5

---
+++ src/main/java/app/views/viewMain.java
@@ -0,0 +1,14 @@
+package app.views;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class viewMain implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
